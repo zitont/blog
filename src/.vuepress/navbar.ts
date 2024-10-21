f49007f20a11e70207aa59f63c9ca649a548c87a
zitont <1456492197@qq.com>
2024-10-21 12:09:47 +0800

    添加node版本控制器nvm的配置安装教程

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -46,9 +46,9 @@ export default navbar([
       "strawberry",
     ],
   },
-  {
-    text: "V2 文档",
-    icon: "book",
-    link: "https://theme-hope.vuejs.press/zh/",
-  },
+  // {
+  //   text: "V2 文档",
+  //   icon: "book",
+  //   link: "https://theme-hope.vuejs.press/zh/",
+  // },
 ]);
